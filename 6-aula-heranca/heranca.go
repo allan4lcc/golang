7a7780b71c54d1da515271ee0f6a732087a77f48
Allan4l <allan4l.cc@gmail.com>
2022-04-26 21:08:20 -0300

    atualizando arquivo heranca.go

--- 6-aula-heranca/heranca.go
+++ 6-aula-heranca/heranca.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+type pessoa struct {
+	nome    string
+	idade   uint8
+	emprego string
+	salario float32
+}
+
+type heraca struct {
+	pessoa
+	estuda string
+	luta   string
+	corre  float32
+}
+
 func main() {
 	fmt.Println("olar heranca!!!")
+	p := pessoa{"paloma", 15, "light", 1500.00}
+	p2 := heraca{p, "Colegio Jardim Alvorada", "judo", 32.5}
+	fmt.Printf("Acessando struct pessoa : %v\n", p2.emprego)
+	fmt.Printf("Acessando struct heraca : %v\n", p2.estuda)
 }
